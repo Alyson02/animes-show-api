1b4a45a790a502b9dafca2682c119ec75d07d767
Alyson02 <alyson.subrim2002@gmail.com>
2023-01-26 23:20:28 -0300

    feat: add middleware to handle erros application

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,12 @@ import express from "express"
 import dotenv from "dotenv"
 dotenv.config()
 import cors from "cors"
+import router from "./routers/"
+import { handleApplicationErrors } from "./middlewares/errorHandlingMiddleware"
 
 const app = express()
 app.use(cors());
+app.use(router);
+app.use(handleApplicationErrors)
 
 app.listen(process.env.PORT, () => console.log(`App listening on port ${process.env.PORT}!`));
---
+++ src/middlewares/errorHandlingMiddleware.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from "express";
+import httpStatus from "http-status";
+
+export function handleApplicationErrors(
+    err: Error,
+    _req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    console.error(err.name);
+
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
+        error: "InternalServerError",
+        message: "Internal Server Error",
+    });
+}
