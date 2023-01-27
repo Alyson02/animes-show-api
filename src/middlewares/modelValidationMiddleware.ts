d472fd951a6b8f354f854d1651b24337121f65d9
Alyson02 <alyson.subrim2002@gmail.com>
2023-01-26 23:21:14 -0300

    fix: typescript any in  generic schema validaiton

--- src/middlewares/modelValidationMiddleware.ts
+++ src/middlewares/modelValidationMiddleware.ts
@@ -1,12 +1,14 @@
-import { NextFunction } from "express";
+import { NextFunction, request } from "express";
 import { ObjectSchema } from "joi";
 import { Request, Response } from "express";
 import result from "http-status"
 
+type ObjectKey = keyof typeof request;
+
 export default function modelValidationMiddleware(schema: ObjectSchema, property: string) {
 
     return (req: Request, res: Response, next: NextFunction) => {
-        const { error } = schema.validate(req[property], { abortEarly: false });
+        const { error } = schema.validate(req[property as ObjectKey], { abortEarly: false });
 
         const valid = error == null;
         if (valid) {
